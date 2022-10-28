b57b2124642cdb34b28dc13ef0616c9715fc3f5a
Allan Gallo <allan.gallo.guerra@gmail.com>
2022-10-28 10:11:44 -0600

    add keymap to close fugitive and push

--- nvim/lua/allan/plugins/fugitive.lua
+++ nvim/lua/allan/plugins/fugitive.lua
@@ -5,3 +5,5 @@ local options = { noremap = true, silent = true }
 keymap.set("n", "<leader>gg", ":G <CR>", options)
 keymap.set("n", "<leader>gp", ":G pull<CR>", options)
 keymap.set("n", "<leader>gs", ":G status<CR>", options)
+keymap.set("n", "<leader>gu", ":G push<CR>", options)
+keymap.set("n", "<leader>gx", ":q<CR>", options)
