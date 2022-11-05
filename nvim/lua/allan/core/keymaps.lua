06f71e7c810d0de40c2667f2075096262d1d32f7
Allan Gallo <allan.gallo.guerra@gmail.com>
2022-11-05 13:17:13 -0600

    Fix the speed when I press `j` in normal mode, and visual mode

--- nvim/lua/allan/core/keymaps.lua
+++ nvim/lua/allan/core/keymaps.lua
@@ -1,37 +1,37 @@
 vim.g.mapleader = " "
 
 local keymap = vim.keymap
+-- local keymap = vim.api.nvim_set_keymap
+local options = { noremap = true, silent = true}
 
 -- general keymaps
-keymap.set("i", "jk", "<ESC>")
-keymap.set("i", "jk", "<ESC>")
-keymap.set("n", "JK", "<ESC>")
-keymap.set("n", "jk", "<ESC>")
-keymap.set("v", "JK", "<ESC>")
-keymap.set("v", "jk", "<ESC>")
+keymap.set("i", "jk", "<ESC>", options)
+keymap.set("i", "JK", "<ESC>", options)
+keymap.set("v", "<leader>JK", "<ESC>", options)
+keymap.set("v", "<leader>jk", "<ESC>", options)
 
 -- Save file
 keymap.set("n", "<leader>ss", ":w<cr>", options)
 keymap.set("i", "<C-s>", "<ESC>:w<cr>", options)
 
 -- Search
-keymap.set("n", "<leader>nh", ":nohl<CR>")
+keymap.set("n", "<leader>nh", ":nohl<CR>", options)
 
 -- Increment & Decrement
-keymap.set("n", "<leader>+", "<C-a>")
-keymap.set("n", "<leader>-", "<C-x>")
+keymap.set("n", "<leader>+", "<C-a>", options)
+keymap.set("n", "<leader>-", "<C-x>", options)
 
 -- split window
-keymap.set("n", "<leader>sv", "<C-w>v") -- vertically
-keymap.set("n", "<leader>sh", "<C-w>s") -- horizontally
-keymap.set("n", "<leader>se", "<C-w>=") -- make split windows equeal width
-keymap.set("n", "<leader>sx", ":close<CR>") -- close current split window
+keymap.set("n", "<leader>sv", "<C-w>v", options) -- vertically
+keymap.set("n", "<leader>sh", "<C-w>s", options) -- horizontally
+keymap.set("n", "<leader>se", "<C-w>=", options) -- make split windows equeal width
+keymap.set("n", "<leader>sx", ":close<CR>", options) -- close current split window
 
 -- tab move
-keymap.set("n", "<leader>to", ":tabnew<CR>") -- open new tab
-keymap.set("n", "<leader>tx", ":tabclose<CR>") -- close current tab
-keymap.set("n", "<leader>tn", ":tabn<CR>") -- go to next tab
-keymap.set("n", "<leader>tp", ":tabp<CR>") -- go to previous tab
+keymap.set("n", "<leader>to", ":tabnew<CR>", options) -- open new tab
+keymap.set("n", "<leader>tx", ":tabclose<CR>", options) -- close current tab
+keymap.set("n", "<leader>tn", ":tabn<CR>", options) -- go to next tab
+keymap.set("n", "<leader>tp", ":tabp<CR>", options) -- go to previous tab
 
 -- Move between screen
 keymap.set("n", "<C-l>", "<C-w>l", options)
@@ -40,8 +40,8 @@ keymap.set("n", "<C-j>", "<C-w>j", options)
 keymap.set("n", "<C-k>", "<C-w>k", options)
 
 -- Moves blocks of code in visual mode
-keymap.set("x", "u", ":move '<-2<CR>gv-gv", options)
-keymap.set("x", "d", ":move '<+1<CR>gv-gv", options)
+keymap.set("x", "U", ":move '<-2<CR>gv-gv", options)
+keymap.set("x", "D", ":move '<+1<CR>gv-gv", options)
 
 -- indentetion
 keymap.set("v", "<", "<gv", options)
