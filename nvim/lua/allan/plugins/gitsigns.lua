cb58b8fcb01abf1b7433de5baf3cc5d4eae50484
Allan Gallo <allan.gallo.guerra@gmail.com>
2022-12-10 11:13:01 -0600

    add keymap to go the next and prev hunk

--- nvim/lua/allan/plugins/gitsigns.lua
+++ nvim/lua/allan/plugins/gitsigns.lua
@@ -4,3 +4,11 @@ if not status then
 end
 
 gitsigns.setup()
+
+
+map('n', '<leader>gj', '<plug>(signify-next-hunk)', options)
+map('n', '<leader>gk', '<plug>(signify-prev-hunk)', options)
+map('n', '<leader>gJ', '9999<leader>gJ', options)
+map('n', '<leader>gK', '9999<leader>gk', options)
+
+
