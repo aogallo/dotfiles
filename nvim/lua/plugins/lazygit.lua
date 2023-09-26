f4ff63c1e9ace9562abccf10775851b2a2a77a62
Allan Gallo <allan.gallo.guerra@gmail.com>
2023-09-26 17:35:07 -0600

    custom configuration for lazygit in neovim

--- nvim/lua/plugins/lazygit.lua
+++ nvim/lua/plugins/lazygit.lua
@@ -8,5 +8,9 @@ return {
     {
       "<leader>gg", ":LazyGit<CR>", desc = "Open Git Tools"
     },
-  }
+  },
+  config = function ()
+    vim.g.lazygit_floating_window_scaling_factor=1
+    vim.g.lazygit_floating_window_border_chars = {'╭','─', '╮', '│', '╯','─', '╰', '│'}
+  end
 }
