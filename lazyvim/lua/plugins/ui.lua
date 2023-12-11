081a89dc81725ce5474f51f0869691cda1771d59
Allan Gallo <allan.gallo.guerra@gmail.com>
2023-12-11 15:03:57 -0600

    change the logo

--- lazyvim/lua/plugins/ui.lua
+++ lazyvim/lua/plugins/ui.lua
@@ -5,4 +5,21 @@ return {
       opts.presets.lsp_doc_border = true
     end,
   },
+  {
+    "nvimdev/dashboard-nvim",
+    event = "VimEnter",
+    opts = function(_, opts)
+      local logo = [[
+        █████╗  ██████╗ 
+        ██╔══██╗██╔════╝ 
+        ███████║██║  ███╗
+        ██╔══██║██║   ██║
+        ██║  ██║╚██████╔╝
+        ╚═╝  ╚═╝ ╚═════╝ 
+      ]]
+
+      logo = string.rep("\n", 9) .. logo .. "\n\n"
+      opts.config.header = vim.split(logo, "\n")
+    end,
+  },
 }
