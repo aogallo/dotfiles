5786bd4b4a3ed3c8863534d3fa23280e64c0603e
Allan Gallo <allan.gallo.guerra@gmail.com>
2024-01-08 09:01:20 -0600

    add border to completitation and documentation window

--- lazyvim/lua/plugins/cmp.lua
+++ lazyvim/lua/plugins/cmp.lua
@@ -23,6 +23,10 @@ return {
           require("luasnip").lsp_expand(args.body)
         end,
       },
+      window = {
+        completion = cmp.config.window.bordered(),
+        documentation = cmp.config.window.bordered(),
+      },
       mapping = cmp.mapping.preset.insert({
         ["<C-n>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
         ["<C-p>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
