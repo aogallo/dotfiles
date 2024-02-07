b03834a4bfc3f5e8eaa3e881a1b94b37769ddccf
Allan Gallo <allan.gallo.guerra@gmail.com>
2024-02-07 10:49:39 -0600

    fix the logo

--- lazyvim/lua/plugins/ui.lua
+++ lazyvim/lua/plugins/ui.lua
@@ -3,6 +3,13 @@ return {
     "folke/noice.nvim",
     opts = function(_, opts)
       opts.presets.lsp_doc_border = true
+      table.insert(opts.routes, {
+        filter = {
+          event = "notify",
+          find = "No information available",
+        },
+        opts = { skip = true },
+      })
     end,
   },
   {
@@ -10,15 +17,16 @@ return {
     event = "VimEnter",
     opts = function(_, opts)
       local logo = [[
-         █████╗  ██████╗
-        ██╔══██╗██╔════╝
-        ███████║██║  ███╗
-        ██╔══██║██║   ██║
-        ██║  ██║╚██████╔╝
-        ╚═╝  ╚═╝ ╚═════╝
-      ]]
+ █████╗  ██████╗
+██╔══██╗██╔════╝
+  ███████║██║  ███╗
+  ██╔══██║██║   ██║
+  ██║  ██║╚██████╔╝
+  ╚═╝  ╚═╝ ╚═════╝
 
-      logo = string.rep("\n", 9) .. logo .. "\n\n"
+]]
+
+      logo = string.rep("\n", 8) .. logo .. "\n\n"
       opts.config.header = vim.split(logo, "\n")
     end,
   },
