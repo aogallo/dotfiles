669a0dac1f782a388961796bbe61374a2901ef22
Allan Gallo <allan.gallo.guerra@gmail.com>
2024-04-30 17:27:58 -0600

    when press ESC remove the highlight words

--- lazyvim/lua/config/keymaps.lua
+++ lazyvim/lua/config/keymaps.lua
@@ -2,6 +2,8 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
+vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
+
 vim.keymap.set("i", "jk", "<ESC>", { desc = "Go to normal mode" })
 vim.keymap.set("i", "JK", "<ESC>", { desc = "Go to normal mode" })
 
@@ -11,7 +13,7 @@ vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 vim.keymap.set("n", "<C-a>", "gg<S-v>G", { desc = "Select all" })
 
 -- Delete single character without copying into register
-vim.keymap.set("n", "x", '"_x', options)
+vim.keymap.set("n", "x", '"_x')
 
 -- Go to end line in insert mode
-vim.keymap.set("i", "<C-e>", "<C-o>$", options)
+vim.keymap.set("i", "<C-e>", "<C-o>$")
