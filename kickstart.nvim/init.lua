d97916243416d1481ab823fc5fb840c21b3a3363
Allan Gallo <allan.gallo.guerra@gmail.com>
2024-05-03 11:10:07 -0600

    add increment and decrement keymap

--- kickstart.nvim/init.lua
+++ kickstart.nvim/init.lua
@@ -157,6 +157,12 @@ vim.opt.scrolloff = 10
 -- [[ Basic Keymaps ]]
 --  See `:help vim.keymap.set()`
 
+-- use `+` and `-` to increment and decrement 8
+vim.keymap.set('n', '+', '<C-a>')
+vim.keymap.set('n', '-', '<C-x>')
+vim.keymap.set('v', '+', 'g<C-a>gv')
+vim.keymap.set('v', '-', 'g<C-x>gv')
+
 -- Set highlight on search, but clear on pressing <Esc> in normal mode
 vim.opt.hlsearch = true
 vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
