c4ad7a3caf077419f52b3606833fb57fdd18ac3b
Allan Gallo <allan.gallo.guerra@gmail.com>
2024-05-10 07:11:18 -0600

    add rutst_analyzer and gols

--- kickstart.nvim/init.lua
+++ kickstart.nvim/init.lua
@@ -156,7 +156,7 @@ vim.opt.scrolloff = 10
 
 -- [[ Basic Keymaps ]]
 --  See `:help vim.keymap.set()`
-
+--
 -- use `+` and `-` to increment and decrement 8
 vim.keymap.set('n', '+', '<C-a>')
 vim.keymap.set('n', '-', '<C-x>')
@@ -598,9 +598,9 @@ require('lazy').setup({
       --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
       local servers = {
         -- clangd = {},
-        -- gopls = {},
+        gopls = {},
         -- pyright = {},
-        -- rust_analyzer = {},
+        rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
         -- Some languages (like typescript) have entire language plugins that can be useful:
