4ede2e2c829f0b9042958b32497a644578844341
Allan Gallo <allan.gallo.guerra@gmail.com>
2024-05-15 10:45:44 -0600

    change the key map to one the oil

--- kickstart.nvim/lua/custom/plugins/oil.lua
+++ kickstart.nvim/lua/custom/plugins/oil.lua
@@ -19,7 +19,7 @@ return {
       vim.keymap.set('n', '-', '<CMD>Oil<CR>', { desc = 'Open parent directory' })
 
       -- Open parent directory in floating window
-      vim.keymap.set('n', '<space>-', require('oil').toggle_float)
+      vim.keymap.set('n', '<space>e', require('oil').toggle_float)
     end,
   },
 }
