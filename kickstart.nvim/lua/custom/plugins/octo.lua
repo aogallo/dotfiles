5fe9623ea7e95d98ac34d54757f3d0665eeab79d
Allan Gallo <allan.gallo.guerra@gmail.com>
2024-05-17 11:24:22 -0600

    add octo to kickstart

---
+++ kickstart.nvim/lua/custom/plugins/octo.lua
@@ -0,0 +1,53 @@
+return {
+  {
+    'pwntester/octo.nvim',
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+      'nvim-telescope/telescope.nvim',
+      'nvim-tree/nvim-web-devicons',
+    },
+    config = function()
+      require('octo').setup { enable_builtin = true }
+      vim.cmd [[hi OctoEditable guibg=noe]]
+    end,
+    keys = {
+      {
+        '<leader>o',
+        '<cmd>Octo<cr>',
+        desc = 'Octo',
+      },
+      {
+        '<leader>gpd',
+        '<cmd>Octo pr draft<cr>',
+        desc = 'Send PR to Draft',
+      },
+      {
+        '<leader>gprs',
+        '<cmd>Octo review start<cr>',
+        desc = 'Start reviewing PR',
+      },
+      {
+        '<leader>gpl',
+        '<cmd>Octo pr list<cr>',
+        desc = 'PR list',
+      },
+      {
+        '<leader>gpb',
+        '<cmd>Octo pr browser<cr>',
+        desc = 'Open PR on the browser',
+      },
+      {
+        '<leader>gpc',
+        '<cmd>Octo pr create<cr>',
+        desc = 'Create a PR ',
+      },
+      {
+        '<leader>gvb',
+        function()
+          require('octo.commands').add_user 'reviewer'
+        end,
+        desc = 'Add the bezlio team as reviewer',
+      },
+    },
+  },
+}
