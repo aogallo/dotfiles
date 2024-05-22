2b21745e99b5a5514c882e0ae053f0724714ac3c
Allan Gallo <allan.gallo.guerra@gmail.com>
2024-05-22 17:16:13 -0600

    node debugger

--- kickstart.nvim/init.lua
+++ kickstart.nvim/init.lua
@@ -90,6 +90,9 @@ P.S. You can delete this when you're done too. It's your config now! :)
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
+-- able to load json5 plugin
+table.insert(vim._so_trails, "/?.dylib")
+
 -- Set to true if you have a Nerd Font installed and selected in the terminal
 vim.g.have_nerd_font = true
 
--- kickstart.nvim/lua/kickstart/plugins/debug.lua
+++ kickstart.nvim/lua/kickstart/plugins/debug.lua
@@ -8,27 +8,98 @@
 
 return {
   -- NOTE: Yes, you can install new plugins here!
-  'mfussenegger/nvim-dap',
+  "mfussenegger/nvim-dap",
   -- NOTE: And you can specify dependencies as well
   dependencies = {
     -- Creates a beautiful debugger UI
-    'rcarriga/nvim-dap-ui',
+    "rcarriga/nvim-dap-ui",
 
     -- Required dependency for nvim-dap-ui
-    'nvim-neotest/nvim-nio',
+    "nvim-neotest/nvim-nio",
 
     -- Installs the debug adapters for you
-    'williamboman/mason.nvim',
-    'jay-babu/mason-nvim-dap.nvim',
+    "williamboman/mason.nvim",
+    "jay-babu/mason-nvim-dap.nvim",
+
+    -- adapter for node application
+    {
+      "mxsdev/nvim-dap-vscode-js",
+      dependencies = {
+        "microsoft/vscode-js-debug",
+        build = "npm install --legacy-peer-deps && npx gulp vsDebugServerBundle && mv dist out",
+      },
+    },
 
     -- Add your own debuggers here
-    'leoluz/nvim-dap-go',
+    "leoluz/nvim-dap-go",
+
+    --dependency to use json5
+    {
+      "Joakker/lua-json5",
+      build = "./install.sh",
+    },
   },
   config = function()
-    local dap = require 'dap'
-    local dapui = require 'dapui'
+    local dap = require("dap")
+    local dapui = require("dapui")
 
-    require('mason-nvim-dap').setup {
+    local node_debugger_path = vim.fn.stdpath("data") .. "/lazy/vscode-js-debug"
+
+    -- install brew install rustup-init
+
+    -- /Users/allangallo/.local/share/nvim-kickstart/lazy/nvim-dap-vscode-js
+    -- debugger_path = "/Users/allangallo/.local/share/nvim-kickstart/lazy/nvim-dap-vscode-js",
+    require("dap-vscode-js").setup({
+      debugger_path = node_debugger_path,
+      adapters = {
+        "chrome",
+        "pwa-node",
+        "pwa-chrome",
+        "pwa-msedge",
+        "node-terminal",
+        "pwa-extensionHost",
+        "node",
+        "chrome",
+      },
+    })
+
+    local js_based_languages = { "typescript", "javascript", "typescriptreact" }
+
+    for _, language in ipairs(js_based_languages) do
+      dap.configurations[language] = {
+        {
+          type = "pwa-node",
+          request = "launch",
+          name = "Launch file",
+          program = "${file}",
+          cwd = "${workspaceFolder}",
+        },
+        {
+          type = "pwa-node",
+          request = "attach",
+          name = "Attach",
+          processId = require("dap.utils").pick_process,
+          cwd = "${workspaceFolder}",
+        },
+        {
+          type = "pwa-chrome",
+          request = "launch",
+          name = 'Start Chrome with "localhost"',
+          url = "http://localhost:3000",
+          webRoot = "${workspaceFolder}",
+          userDataDir = "${workspaceFolder}/.vscode/vscode-chrome-debug-userdatadir",
+        },
+      }
+    end
+
+    require("dap.ext.vscode").load_launchjs(nil, {
+      ["pwa-node"] = js_based_languages,
+      ["node"] = js_based_languages,
+      ["chrome"] = js_based_languages,
+      ["pwa-chrome"] = js_based_languages,
+    })
+
+    require("mason-nvim-dap").setup({
       -- Makes a best effort to setup the various debuggers with
       -- reasonable debug configurations
       automatic_setup = true,
@@ -43,48 +114,48 @@ return {
         -- Update this to ensure that you have the debuggers for the langs you want
         -- 'delve',
       },
-    }
+    })
 
     -- Basic debugging keymaps, feel free to change to your liking!
-    vim.keymap.set('n', '<F5>', dap.continue, { desc = 'Debug: Start/Continue' })
-    vim.keymap.set('n', '<F1>', dap.step_into, { desc = 'Debug: Step Into' })
-    vim.keymap.set('n', '<F2>', dap.step_over, { desc = 'Debug: Step Over' })
-    vim.keymap.set('n', '<F3>', dap.step_out, { desc = 'Debug: Step Out' })
-    vim.keymap.set('n', '<leader>b', dap.toggle_breakpoint, { desc = 'Debug: Toggle Breakpoint' })
-    vim.keymap.set('n', '<leader>B', function()
-      dap.set_breakpoint(vim.fn.input 'Breakpoint condition: ')
-    end, { desc = 'Debug: Set Breakpoint' })
+    vim.keymap.set("n", "<F5>", dap.continue, { desc = "Debug: Start/Continue" })
+    vim.keymap.set("n", "<F1>", dap.step_into, { desc = "Debug: Step Into" })
+    vim.keymap.set("n", "<F2>", dap.step_over, { desc = "Debug: Step Over" })
+    vim.keymap.set("n", "<F3>", dap.step_out, { desc = "Debug: Step Out" })
+    vim.keymap.set("n", "<leader>b", dap.toggle_breakpoint, { desc = "Debug: Toggle Breakpoint" })
+    vim.keymap.set("n", "<leader>B", function()
+      dap.set_breakpoint(vim.fn.input("Breakpoint condition: "))
+    end, { desc = "Debug: Set Breakpoint" })
 
     -- Dap UI setup
     -- For more information, see |:help nvim-dap-ui|
-    dapui.setup {
+    dapui.setup({
       -- Set icons to characters that are more likely to work in every terminal.
       --    Feel free to remove or use ones that you like more! :)
       --    Don't feel like these are good choices.
-      icons = { expanded = '▾', collapsed = '▸', current_frame = '*' },
+      icons = { expanded = "▾", collapsed = "▸", current_frame = "*" },
       controls = {
         icons = {
-          pause = '⏸',
-          play = '▶',
-          step_into = '⏎',
-          step_over = '⏭',
-          step_out = '⏮',
-          step_back = 'b',
-          run_last = '▶▶',
-          terminate = '⏹',
-          disconnect = '⏏',
+          pause = "⏸",
+          play = "▶",
+          step_into = "⏎",
+          step_over = "⏭",
+          step_out = "⏮",
+          step_back = "b",
+          run_last = "▶▶",
+          terminate = "⏹",
+          disconnect = "⏏",
         },
       },
-    }
+    })
 
     -- Toggle to see last session result. Without this, you can't see session output in case of unhandled exception.
-    vim.keymap.set('n', '<F7>', dapui.toggle, { desc = 'Debug: See last session result.' })
+    vim.keymap.set("n", "<F7>", dapui.toggle, { desc = "Debug: See last session result." })
 
-    dap.listeners.after.event_initialized['dapui_config'] = dapui.open
-    dap.listeners.before.event_terminated['dapui_config'] = dapui.close
-    dap.listeners.before.event_exited['dapui_config'] = dapui.close
+    dap.listeners.after.event_initialized["dapui_config"] = dapui.open
+    dap.listeners.before.event_terminated["dapui_config"] = dapui.close
+    dap.listeners.before.event_exited["dapui_config"] = dapui.close
 
     -- Install golang specific config
-    require('dap-go').setup()
+    require("dap-go").setup()
   end,
 }
