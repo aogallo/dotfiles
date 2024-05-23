c38043707766289238190e758c169477a90f60c5
Allan Gallo <allan.gallo.guerra@gmail.com>
2024-05-22 18:59:40 -0600

    fix handlers

--- kickstart.nvim/lua/kickstart/plugins/debug.lua
+++ kickstart.nvim/lua/kickstart/plugins/debug.lua
@@ -83,24 +83,13 @@ return {
 
       -- You can provide additional configuration to the handlers,
       -- see mason-nvim-dap README for more information
-      handlers = {
-
-        javascript = function(config)
-          config.adapters = {
-
-            type = "executable",
-            command = vim.fn.exepath("vscode-js-debug"),
-          }
-          require("mason-nvim-dap").default_setup(config)
-        end,
-      },
+      handlers = {},
 
       -- You'll need to check that you have the required things installed
       -- online, please don't ask me how to install them :)
       ensure_installed = {
         -- Update this to ensure that you have the debuggers for the langs you want
         -- 'delve',
-        "chrome",
       },
     })
 
