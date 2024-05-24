90f5214a49308e949185f613a04a5c1d40fb3e58
Allan Gallo <allan.gallo.guerra@gmail.com>
2024-05-24 15:04:11 -0600

    add tailwindcss to lsp

--- kickstart.nvim/init.lua
+++ kickstart.nvim/init.lua
@@ -362,7 +362,7 @@ require("lazy").setup({
         "nvim-telescope/telescope-file-browser.nvim",
         dependencies = { "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim" },
       },
-      "folke/trouble.nvim"
+      "folke/trouble.nvim",
     },
     config = function()
       -- Telescope is a fuzzy finder that comes with a lot of different things that
@@ -455,23 +455,22 @@ require("lazy").setup({
         builtin.find_files({ cwd = vim.fn.stdpath("config") })
       end, { desc = "[S]earch [N]eovim files" })
 
-
       local actions = require("telescope.actions")
-local open_with_trouble = require("trouble.sources.telescope").open
+      local open_with_trouble = require("trouble.sources.telescope").open
 
--- Use this to add more results without clearing the trouble list
-local add_to_trouble = require("trouble.sources.telescope").add
+      -- Use this to add more results without clearing the trouble list
+      local add_to_trouble = require("trouble.sources.telescope").add
 
-local telescope = require("telescope")
+      local telescope = require("telescope")
 
-telescope.setup({
-  defaults = {
-    mappings = {
-      i = { ["<c-t>"] = open_with_trouble },
-      n = { ["<c-t>"] = open_with_trouble },
-    },
-  },
-})
+      telescope.setup({
+        defaults = {
+          mappings = {
+            i = { ["<c-t>"] = open_with_trouble },
+            n = { ["<c-t>"] = open_with_trouble },
+          },
+        },
+      })
     end,
   },
 
@@ -716,6 +715,7 @@ telescope.setup({
             },
           },
         },
+        tailwindcss = {},
       }
 
       -- Ensure the servers and tools above are installed
@@ -733,6 +733,7 @@ telescope.setup({
         "stylua", -- Used to format Lua code
         "lua_ls",
         "tsserver",
+        "tailwindcss",
       })
 
       require("mason-tool-installer").setup({ ensure_installed = ensure_installed })
