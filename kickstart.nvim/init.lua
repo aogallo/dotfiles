f9f498ecfcb7c765f10469aef6188dfdf1ead5fe
Allan Gallo <allan.gallo.guerra@gmail.com>
2024-05-28 13:19:15 -0600

    set follow current file and buffer to true

--- kickstart.nvim/init.lua
+++ kickstart.nvim/init.lua
@@ -658,7 +658,7 @@ require("lazy").setup({
       --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
       local servers = {
         -- clangd = {},
-        -- gopls = {},
+        gopls = {},
         -- pyright = {},
         csharp_ls = {},
         rust_analyzer = {},
--- kickstart.nvim/lua/kickstart/plugins/neo-tree.lua
+++ kickstart.nvim/lua/kickstart/plugins/neo-tree.lua
@@ -2,24 +2,30 @@
 -- https://github.com/nvim-neo-tree/neo-tree.nvim
 
 return {
-  'nvim-neo-tree/neo-tree.nvim',
+  "nvim-neo-tree/neo-tree.nvim",
   -- branch = 'v3.x',
-  version = '*',
+  version = "*",
   dependencies = {
-    'nvim-lua/plenary.nvim',
-    'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
-    'MunifTanjim/nui.nvim',
+    "nvim-lua/plenary.nvim",
+    "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+    "MunifTanjim/nui.nvim",
   },
-  cmd = 'Neotree',
+  cmd = "Neotree",
   keys = {
-    { ';e', ':Neotree toggle<CR>', { desc = 'NeoTree toggle' } },
+    { ";e", ":Neotree toggle<CR>", { desc = "NeoTree toggle" } },
   },
   opts = {
     filesystem = {
+      follow_current_file = {
+        enabled = true,
+      },
       window = {
         mappings = {
-          ['\\'] = 'close_window',
+          ["\\"] = "close_window",
         },
+      },
+      buffers = {
+        follow_current_file = { enabled = true },
       },
     },
   },
