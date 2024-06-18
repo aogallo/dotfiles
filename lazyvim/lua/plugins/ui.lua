f0a86450948f58f2f37e57f00bd6615f4b7474a3
Allan Gallo <allan.gallo.guerra@gmail.com>
2024-06-18 07:55:25 -0600

    lazyvim remove dashboard

--- lazyvim/lua/plugins/ui.lua
+++ lazyvim/lua/plugins/ui.lua
@@ -12,22 +12,4 @@ return {
       })
     end,
   },
-  {
-    "nvimdev/dashboard-nvim",
-    event = "VimEnter",
-    opts = function(_, opts)
-      local logo = [[
- █████╗  ██████╗
-██╔══██╗██╔════╝
-  ███████║██║  ███╗
-  ██╔══██║██║   ██║
-  ██║  ██║╚██████╔╝
-  ╚═╝  ╚═╝ ╚═════╝
-
-]]
-
-      logo = string.rep("\n", 8) .. logo .. "\n\n"
-      opts.config.header = vim.split(logo, "\n")
-    end,
-  },
 }
