4d483f5c5a6a42aca6bd60046535d8f5bc0afbcb
Allan Gallo <allan.gallo.guerra@gmail.com>
2024-06-18 19:57:13 -0600

    add gitsigns, octo keympas, cmp change the enter behaivor

---
+++ lazyvim/lua/plugins/gitsigns.lua
@@ -0,0 +1,8 @@
+return {
+  "lewis6991/gitsigns.nvim",
+  event = "LazyFile",
+  opts = {
+    numhl = true,
+    current_line_blame = true,
+  },
+}
--- lazyvim/lua/plugins/octo.lua
+++ lazyvim/lua/plugins/octo.lua
@@ -3,5 +3,7 @@ return {
   keys = {
     -- create a pull request
     { "<leader>gC", "<cmd>Octo pr create<CR>", desc = "Create Pull Request (Octo)" },
+    -- set draft pr
+    { "<leader>gD", "<cmd>Octo pr draft<cr>", desc = "Send PR to Draft" },
   },
 }
