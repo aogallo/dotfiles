172ebfab037205ea684fb2cfcaa832de269ecfcc
Allan Gallo <allan.gallo.guerra@gmail.com>
2024-07-04 17:33:15 -0600

    fix keympas

--- lazyvim/lua/plugins/octo.lua
+++ lazyvim/lua/plugins/octo.lua
@@ -4,10 +4,10 @@ return {
     -- create a pull request
     { "<leader>go", "<cmd>Octo pr create<CR>", desc = "Create Pull Request (Octo)" },
     -- set draft pr
-    { "<leader>gD", "<cmd>Octo pr draft<cr>", desc = "Send PR to Draft" },
+    { "<leader>gD", "<cmd>Octo pr draft<cr>", desc = "Send PR to Draft (Octo)" },
     -- open pull request in browser
-    { "<leader>gD", "<cmd>Octo pr browser<cr>", desc = "Open Pull Request in browser" },
+    { "<leader>ga", "<cmd>Octo pr browser<cr>", desc = "Open Pull Request in browser (Octo)" },
     -- copy pr url to clipboard
-    { "<leader>gu", "<cmd>Octo pr url<cr>", desc = "Copy PR URL to clipboard" },
+    { "<leader>gu", "<cmd>Octo pr url<cr>", desc = "Copy PR URL to clipboard (Octo)" },
   },
 }
