2ff75a2167a4cc1d0b38fe828fa83ecb24fd7598
Allan Gallo <allan.gallo.guerra@gmail.com>
2024-07-04 17:21:17 -0600

    add keymaps for octo

--- lazyvim/lua/plugins/octo.lua
+++ lazyvim/lua/plugins/octo.lua
@@ -2,8 +2,12 @@ return {
   "pwntester/octo.nvim",
   keys = {
     -- create a pull request
-    { "<leader>gC", "<cmd>Octo pr create<CR>", desc = "Create Pull Request (Octo)" },
+    { "<leader>go", "<cmd>Octo pr create<CR>", desc = "Create Pull Request (Octo)" },
     -- set draft pr
     { "<leader>gD", "<cmd>Octo pr draft<cr>", desc = "Send PR to Draft" },
+    -- open pull request in browser
+    { "<leader>gD", "<cmd>Octo pr browser<cr>", desc = "Open Pull Request in browser" },
+    -- copy pr url to clipboard
+    { "<leader>gu", "<cmd>Octo pr url<cr>", desc = "Copy PR URL to clipboard" },
   },
 }
