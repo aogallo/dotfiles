650d247e82bbb851101a2059a6323fb3af92dd04
Allan Gallo <allan.gallo.guerra@gmail.com>
2024-09-25 10:08:33 -0600

    fix npm

--- kickstart.nvim/lua/custom/plugins/markdown-preview.lua
+++ kickstart.nvim/lua/custom/plugins/markdown-preview.lua
@@ -3,7 +3,7 @@ return {
   "iamcco/markdown-preview.nvim",
   cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
   ft = { "markdown" },
-  build = "cd app && yarn install",
+  build = "cd app && npm install",
   init = function()
     vim.g.mkdp_filetypes = { "markdown" }
   end,
