a435c1fae42cc9018f8f8a7523f9dfa1248d9f7f
Allan Gallo <allan.gallo.guerra@gmail.com>
2024-09-25 10:04:41 -0600

    install markdown preview without yarn o npm

--- kickstart.nvim/lua/custom/plugins/markdown-preview.lua
+++ kickstart.nvim/lua/custom/plugins/markdown-preview.lua
@@ -1,12 +1,11 @@
--- install with yarn or npm
+-- install without yarn or npm
 return {
   "iamcco/markdown-preview.nvim",
   cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-  build = "cd app && yarn install",
-  init = function()
-    vim.g.mkdp_filetypes = { "markdown" }
-  end,
   ft = { "markdown" },
+  build = function()
+    vim.fn["mkdp#util#install"]()
+  end,
   keys = {
     {
       "<leader>mp",
