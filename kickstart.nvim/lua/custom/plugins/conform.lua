284329e8fbc07fea609ac8fca56d8419313f9764
Allan Gallo <allan.gallo.guerra@gmail.com>
2024-09-26 16:28:37 -0600

    comment formatters

--- kickstart.nvim/lua/custom/plugins/conform.lua
+++ kickstart.nvim/lua/custom/plugins/conform.lua
@@ -24,25 +24,25 @@ return { -- Autoformat
         lsp_fallback = not disable_filetypes[vim.bo[bufnr].filetype],
       }
     end,
-    formatters = {
-      ["markdown-toc"] = {
-        condition = function(_, ctx)
-          for _, line in ipairs(vim.api.nvim_buf_get_lines(ctx.buf, 0, -1, false)) do
-            if line:find("<!%-%- toc %-%->") then
-              return true
-            end
-          end
-        end,
-      },
-      ["markdownlint-cli2"] = {
-        condition = function(_, ctx)
-          local diag = vim.tbl_filter(function(d)
-            return d.source == "markdownlint"
-          end, vim.diagnostic.get(ctx.buf))
-          return #diag > 0
-        end,
-      },
-    },
+    -- formatters = {
+    --   ["markdown-toc"] = {
+    --     condition = function(_, ctx)
+    --       for _, line in ipairs(vim.api.nvim_buf_get_lines(ctx.buf, 0, -1, false)) do
+    --         if line:find("<!%-%- toc %-%->") then
+    --           return true
+    --         end
+    --       end
+    --     end,
+    --   },
+    --   ["markdownlint-cli2"] = {
+    --     condition = function(_, ctx)
+    --       local diag = vim.tbl_filter(function(d)
+    --         return d.source == "markdownlint"
+    --       end, vim.diagnostic.get(ctx.buf))
+    --       return #diag > 0
+    --     end,
+    --   },
+    -- },
     formatters_by_ft = {
       lua = { "stylua" },
       -- Conform can also run multiple formatters sequentially
