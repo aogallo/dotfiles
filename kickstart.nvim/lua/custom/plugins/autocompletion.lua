073717a8618e1f6f5ef88ae7c7d4b024dcac70e5
Allan Gallo <allan.gallo.guerra@gmail.com>
2025-01-09 10:17:38 -0600

    remove for wirters

--- kickstart.nvim/lua/custom/plugins/autocompletion.lua
+++ kickstart.nvim/lua/custom/plugins/autocompletion.lua
@@ -64,41 +64,5 @@ return {
       },
     },
     signature = { enabled = true, window = { border = "single" } },
-    sources = {
-      providers = {
-        buffer = {
-          transform_items = function(ctx, items)
-            local keyword = ctx.get_keyword()
-            local correct, case
-            if keyword:match("^%l") then
-              correct = "^%u%l+$"
-              case = string.lower
-            elseif keyword:match("^%u") then
-              correct = "^%l+$"
-              case = string.upper
-            else
-              return items
-            end
-
-            -- avoid duplicates from the corrections
-            local seen = {}
-            local out = {}
-            for _, item in ipairs(items) do
-              local raw = item.insertText
-              if raw:match(correct) then
-                local text = case(raw:sub(1, 1)) .. raw:sub(2)
-                item.insertText = text
-                item.label = text
-              end
-              if not seen[item.insertText] then
-                seen[item.insertText] = true
-                table.insert(out, item)
-              end
-            end
-            return out
-          end,
-        },
-      },
-    },
   },
 }
