305b57f4a1c5f01c86ece5d9b041946dd42118ac
Allan Gallo <allan.gallo.guerra@gmail.com>
2025-01-31 08:50:57 -0600

    add blink

--- kickstart.nvim/lua/custom/plugins/lsp.lua
+++ kickstart.nvim/lua/custom/plugins/lsp.lua
@@ -161,6 +161,7 @@ return { -- LSP Configuration & Plugins
     --  When you add nvim-cmp, luasnip, etc. Neovim now has *more* capabilities.
     --  So, we create new capabilities with nvim cmp, and then broadcast that to the servers.
     local capabilities = vim.lsp.protocol.make_client_capabilities()
+
     capabilities = vim.tbl_deep_extend("force", capabilities, require("blink.cmp").get_lsp_capabilities())
 
     -- Enable the following language servers
