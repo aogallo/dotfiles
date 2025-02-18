13c3dded31428bb2f62cbebc958f0a3e9096fd95
Allan Gallo <allan.gallo.guerra@gmail.com>
2025-02-18 13:46:59 -0600

    images conf

--- lazyvim/lua/plugins/images.lua
+++ lazyvim/lua/plugins/images.lua
@@ -2,6 +2,12 @@ return {
   "folke/snacks.nvim",
   ---@type snacks.config
   opts = {
-    image = {},
+    image = {
+      enabled = true,
+      inline = vim.g.neovim_mode == "skitty" and true or false,
+      float = true,
+      max_width = vim.g.neovim_mode == "skitty" and 20 or 60,
+      max_height = vim.g.neovim_mode == "skitty" and 10 or 30,
+    },
   },
 }
