13a3fc1536088340a0440aab19ba90d6b7ce6bd6
aogallo <allan.gallo.guerra@gmail.com>
2025-03-05 13:28:57 -0600

    changes the keymap

--- kickstart.nvim/init.lua
+++ kickstart.nvim/init.lua
@@ -161,7 +161,7 @@ require("lazy").setup({
       vim.keymap.set("n", "<leader>sf", builtin.find_files, { desc = "[S]earch [F]iles" })
       vim.keymap.set("n", "<leader>ss", builtin.builtin, { desc = "[S]earch [S]elect Telescope" })
       vim.keymap.set("n", "<leader>sw", builtin.grep_string, { desc = "[S]earch current [W]ord" })
-      vim.keymap.set("n", "<leader>sg", builtin.live_grep, { desc = "[S]earch by [G]rep" })
+      -- vim.keymap.set("n", "<leader>sg", builtin.live_grep, { desc = "[S]earch by [G]rep" })
       vim.keymap.set("n", "<leader>sd", builtin.diagnostics, { desc = "[S]earch [D]iagnostics" })
       vim.keymap.set("n", "<leader>sr", builtin.resume, { desc = "[S]earch [R]esume" })
       vim.keymap.set("n", "<leader>s.", builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
@@ -191,7 +191,7 @@ require("lazy").setup({
         builtin.find_files({ cwd = vim.fn.stdpath("config") })
       end, { desc = "[S]earch [N]eovim files" })
 
-      vim.keymap.set("n", "<leader>sf", function(opts)
+      vim.keymap.set("n", "<leader>sg", function(opts)
         local pickers = require("telescope.pickers")
         local finders = require("telescope.finders")
         local make_entry = require("telescope.make_entry")
